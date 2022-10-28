acb3a9ab3668542cdca456df361b544de885bd3f
Udesh Habaraduwa <udesh86@gmail.com>
2022-10-28 09:40:25 +0200

    Adds functionality to sample position at 5 Hz frequency

--- obs/examples.py
+++ obs/examples.py
@@ -40,7 +40,10 @@ def GetPosition(matches = []):
     ctr = 1;
     positionDict = {}
     #print("test")
+    ## TODO : Parallelize this
     for file in replayFiles[1:]:
+        if ctr > 5:
+            break
         print("Parsing file {}/{}..".format(ctr,len(replayFiles[1:])), end="\r", flush=True)
         # if os.path.getsize(file) == 0:
         #     print('File {} is empty'.format(file))
@@ -54,23 +57,63 @@ def GetPosition(matches = []):
         #print(type(output))
         p_status = p.wait()
         ctr+=1
-    matchCoordsDict = {}
+    results = {}
+    print()
+    print("Done")
+    ctr = 1
+    # TODO : Parallelize this
     for matchId in positionDict.keys():
         try:
             # skip the first 10 rows which gives player assignments, not location
             # TODO : Implement change so that dynamically determines last position entry
+            print("Building dataframes {}/{}..".format(ctr, len(positionDict.keys())), end="\r", flush=True)
             series = pd.Series(positionDict[matchId])
             firstPos = series.str.contains('_').idxmax()
             series = series[firstPos:].reset_index(drop = True)
             df = series.str.split(',', expand=True)
             df = df.rename(columns={0: "Player", 1: "X", 2: "Y", 3: "Z", 4: "Time"})
             # skip the last row which gives info about how long the parsing took
-            matchCoordsDict[matchId] = df.iloc[0:-2]
+            results[matchId] = df.iloc[0:-2]
+            ctr += 1
         except:
             print(matchId, series)
+            ctr += 1
             continue
-    return matchCoordsDict
-
+    print()
+    print("Done")
+    matchDict = {}
+    matchCtr = 1
+    for match in results:
+        print("Sampling match {}/{}".format(matchCtr, len(results)),end="\r", flush=True)
+        results[match]['Time'] = pd.to_numeric(results[match]['Time'])
+        firstTs = abs(results[match].iloc[0]['Time'])
+        # convert each recording to milliseconds
+        results[match]['Time'] = (results[match]['Time'] + firstTs) * 1000
+        tEnd = results[match].iloc[-1]['Time']
+        # number of 200 ms invervals
+        ints = int(np.ceil(tEnd / 200))
+        # equally spaced (200 ms intervals) values from 0 to tEnd
+        timeArray = [i * 200 for i in range(1, ints + 1)]
+        timeIndex = np.zeros(ints)
+        playerDict = {}
+        for i in range(10):
+            player = 'Player_0{}'.format(i)
+            playerDf = results[match][results[match]['Player'] == player].reset_index()
+            arrayCtr = 0;
+            startTime = 0;
+            for i in range(1, len(playerDf)):
+                if (round(playerDf['Time'].iloc[i], 3) >= startTime):
+                    timeIndex[arrayCtr] = playerDf['index'][i - 1]
+                    startTime += 200
+                    arrayCtr += 1
+            df = playerDf[playerDf['index'].isin(timeIndex[:arrayCtr + 1])].drop(columns="index").reset_index(drop=True)
+            df = pd.concat([df, df.iloc[[-1] * (len(timeArray) - len(df))]])
+            df['Time'] = timeArray
+            playerDict[player] = df
+        matchCtr += 1
+        matchDict[match] = playerDict
+    print()
+    return matchDict
     # This makes the wait possible
     # p_status = p.wait()
     # print(type(output))
