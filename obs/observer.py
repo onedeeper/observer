204ae2e63e455bf4ad9316cfab5c96041060f869
Udesh Habaraduwa <udesh86@gmail.com>
2022-12-21 13:52:11 +0100

    Seperates out functions in observer, adds exceptiond handling for sampling

--- obs/observer.py
+++ obs/observer.py
@@ -35,24 +35,40 @@ def GetPosition(matches = []):
         if not all(elem in files_in_dir for elem in replayFiles):
             print("Specified replay files not found. Downloading replays.")
             obs.utils.DownloadReplays(matches)
-    ctr = 1;
+    positionDict = ParseFiles(replayFiles)
+    results = BuildDataFrames(positionDict)
+    sampledResults = SampleFromMatches(results)
+    return sampledResults
+def ParseFiles(replayFiles : list):
+
+    """
+    Parses replay files and extracts position data for each player across a match using
+    the clarity parser"""
     positionDict = {}
-    ## TODO : Parallelize this
+    ctr = 1
     for file in replayFiles:
         print("Parsing file {}/{}..".format(ctr,len(replayFiles)))
         curDir = "{}/{}".format(subprocess.run("pwd", capture_output=True).stdout.decode("utf-8").replace(" ","\ ").strip(),file)
         p = subprocess.Popen("java -jar target/position.one-jar.jar '{}'".format(shlex.quote(curDir)), shell = True,cwd = './clarity-examples', stdout=subprocess.PIPE)
         output, err = p.communicate()
-        print("Output : {}".format(output.decode("utf-8")))
+        #print("Output : {}".format(output.decode("utf-8")))
         if output.decode("utf-8") == "":
             print("Java Runtime Error. Please make sure a Java Runtime Environment is installed, path is defined, etc.")
             return
         positionDict[file.split('.')[0]] = output.decode('utf-8').split('\n')
         p_status = p.wait()
         ctr+=1
-    results = {}
     print()
     print("Done")
+    return positionDict
+
+def BuildDataFrames(positionDict : dict):
+    """
+    builds dataframes from the position data extracted from the replay files
+    :param positionDict:
+    :return:
+    """
+    results = {}
     ctr = 1
     # TODO : Parallelize this
     for matchId in positionDict.keys():
@@ -62,11 +78,11 @@ def GetPosition(matches = []):
             print("Building dataframes {}/{}..".format(ctr, len(positionDict.keys())))
             series = pd.Series(positionDict[matchId])
             firstPos = series.str.contains('_').idxmax()
-            series = series[firstPos:].reset_index(drop = True)
+            series = series[firstPos:].reset_index(drop=True)
             df = series.str.split(',', expand=True)
             df = df.rename(columns={0: "player", 1: "x", 2: "y", 3: "z", 4: "time", 5: "mana", 6: "mana_regen",
-                                    7: "max_mana", 8:"hp", 9 : "hp_regen", 10 : "max_hp", 11 : "xp", 12 : "level",
-                                    13 : "str", 14 : "int",15 : "agi"})
+                                    7: "max_mana", 8: "hp", 9: "hp_regen", 10: "max_hp", 11: "xp", 12: "level",
+                                    13: "str", 14: "int", 15: "agi"})
             # skip the last row which gives info about how long the parsing took
             results[matchId] = df.iloc[0:-2]
             ctr += 1
@@ -76,36 +92,50 @@ def GetPosition(matches = []):
             continue
     print()
     print("Done")
+    return results
+
+def SampleFromMatches(results : dict):
+    """
+    Samples at 200ms for each player in each match
+    :param
+    :param results:
+    :return:
+    """
     matchDict = {}
     matchCtr = 1
-    for match in results:
-        print("Sampling match {}/{}".format(matchCtr, len(results)))
-        results[match]['time'] = pd.to_numeric(results[match]['time'])
-        firstTs = abs(results[match].iloc[0]['time'])
-        # convert each recording to milliseconds
-        results[match]['time'] = (results[match]['time'] + firstTs) * 1000
-        tEnd = results[match].iloc[-1]['time']
-        # number of 200 ms invervals
-        ints = int(np.ceil(tEnd / 200))
-        # equally spaced (200 ms intervals) values from 0 to tEnd
-        timeArray = [i * 200 for i in range(1, ints + 1)]
-        timeIndex = np.zeros(ints)
-        playerDict = {}
-        for i in range(10):
-            player = 'Player_0{}'.format(i)
-            playerDf = results[match][results[match]['player'] == player].reset_index()
-            arrayCtr = 0;
-            startTime = 0;
-            for i in range(1, len(playerDf)):
-                if (round(playerDf['time'].iloc[i], 3) >= startTime):
-                    timeIndex[arrayCtr] = playerDf['index'][i - 1]
-                    startTime += 200
-                    arrayCtr += 1
-            df = playerDf[playerDf['index'].isin(timeIndex[:arrayCtr + 1])].drop(columns="index").reset_index(drop=True)
-            df = pd.concat([df, df.iloc[[-1] * (len(timeArray) - len(df))]])
-            df['time'] = timeArray
-            playerDict[player] = df
+    try:
+        for match in results:
+            print("Sampling match {}/{}".format(matchCtr, len(results)))
+            results[match]['time'] = pd.to_numeric(results[match]['time'])
+            firstTs = abs(results[match].iloc[0]['time'])
+            # convert each recording to milliseconds
+            results[match]['time'] = (results[match]['time'] + firstTs) * 1000
+            tEnd = results[match].iloc[-1]['time']
+            # number of 200 ms invervals
+            ints = int(np.ceil(tEnd / 200))
+            # equally spaced (200 ms intervals) values from 0 to tEnd
+            timeArray = [i * 200 for i in range(1, ints + 1)]
+            timeIndex = np.zeros(ints)
+            playerDict = {}
+            for i in range(10):
+                player = 'Player_0{}'.format(i)
+                playerDf = results[match][results[match]['player'] == player].reset_index()
+                arrayCtr = 0;
+                startTime = 0;
+                for i in range(1, len(playerDf)):
+                    if (round(playerDf['time'].iloc[i], 3) >= startTime):
+                        timeIndex[arrayCtr] = playerDf['index'][i - 1]
+                        startTime += 200
+                        arrayCtr += 1
+                df = playerDf[playerDf['index'].isin(timeIndex[:arrayCtr + 1])].drop(columns="index").reset_index(drop=True)
+                df = pd.concat([df, df.iloc[[-1] * (len(timeArray) - len(df))]])
+                df['time'] = timeArray
+                playerDict[player] = df
+            matchCtr += 1
+            matchDict[match] = playerDict
+        print()
+        return matchDict
+    except:
+        print("Error sampling match {}. Dataframe of length {}".format(match, len(results[match])))
         matchCtr += 1
-        matchDict[match] = playerDict
-    print()
-    return matchDict
+        return
